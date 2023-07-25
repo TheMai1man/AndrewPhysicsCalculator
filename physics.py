31e6b2154ab645a15083a948b48473dad775425b
TheMai1man <liamashercain@gmail.com>
2023-07-25 15:16:03 +0800

    third commit

--- physics.py
+++ physics.py
@@ -1,7 +1,23 @@
-
-
-def read_vars(filename):
+def read_vars(input_file_name):
     """Read the next line from the variable value input file
         Each line specifies values to assign each variable in the equation 
         The input file will be in CSV format.
     """
+    #open file
+    #read the next line for values
+    #return the values
+
+
+def physics_equation(a, b ,c):
+    """Insert physics equation here
+    """
+
+    result = equation(a, b, c)
+
+    return result
+
+
+def write_result(output_file_name, result):
+    """Write to the output file with the result of the equation
+    """
+
