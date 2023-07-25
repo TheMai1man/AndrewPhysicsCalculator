5fff5541a2c0b1474059712f8a8a6aa788c08f96
TheMai1man <liamashercain@gmail.com>
2023-07-25 15:06:12 +0800

    second commit

---
+++ physics.py
@@ -0,0 +1,7 @@
+
+
+def read_vars(filename):
+    """Read the next line from the variable value input file
+        Each line specifies values to assign each variable in the equation 
+        The input file will be in CSV format.
+    """
