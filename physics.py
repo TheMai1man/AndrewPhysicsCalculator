5b2510939fd232e1edb79eb12269023fcdb6a462
TheMai1man <liamashercain@gmail.com>
2023-07-25 16:12:10 +0800

    25/07/23 final commit of the day

--- physics.py
+++ physics.py
@@ -1,6 +1,8 @@
 
 def main():
-    """
+    """For each line in the input file
+       1. Read the line and use values in the equation
+       2. write the result to the output file
     """
     input_file = open('values.csv', 'r', encoding="utf-8")
 
@@ -14,12 +16,11 @@ def main():
 def equation(a, b ,c):
     """Insert physics equation here
     """
-    return #equation result
+    return a + b + c #equation result
 
 
 def write_result(result):
     """Write to the output file with the result of the equation
     """
-    #open file
     with open('results.csv', 'a', encoding="utf-8") as f:
         f.write( str(result) + ',' )
