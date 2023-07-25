e97090e9c18a077a007532faeba3d0e57ca6f2ce
TheMai1man <liamashercain@gmail.com>
2023-07-25 15:56:01 +0800

    draft file IO

--- physics.py
+++ physics.py
@@ -1,23 +1,25 @@
-def read_vars(input_file_name):
-    """Read the next line from the variable value input file
-        Each line specifies values to assign each variable in the equation 
-        The input file will be in CSV format.
+
+def main():
     """
-    #open file
-    #read the next line for values
-    #return the values
+    """
+    input_file = open('values.csv', 'r', encoding="utf-8")
 
+    for line in input_file:
+        value = line.split(',')
+        result = equation(value[0], value[1], value[2])
+        write_result(result)
 
-def physics_equation(a, b ,c):
+        
+
+def equation(a, b ,c):
     """Insert physics equation here
     """
-
-    result = equation(a, b, c)
-
-    return result
+    return #equation result
 
 
-def write_result(output_file_name, result):
+def write_result(result):
     """Write to the output file with the result of the equation
     """
-
+    #open file
+    with open('results.csv', 'a', encoding="utf-8") as f:
+        f.write( str(result) + ',' )
